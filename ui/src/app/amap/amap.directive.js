2036ccfb0ca735c85578bb2b9bc2b7205f45d646
GinMu <gin.musheng@gmail.com>
2017-10-13 14:20:27 +0800

    Update AMap directive

--- ui/src/app/amap/amap.directive.js
+++ ui/src/app/amap/amap.directive.js
@@ -5,17 +5,21 @@
 /* eslint-disable import/no-unresolved, import/default */
 
 
-/*@ngInject*/
+
 import AMap from 'AMap';
-export default function AmapDirective() {
-    
+
+/*@ngInject*/
+export default function AmapDirective($log) {
+    let amap,
+        geolocation;
     return {
         restrict: "E",
-        template: '<div id="container" style="height: 500px;"></div>',
-        link: function() {
-            var map = new AMap.Map('container');
-            map.plugin('AMap.Geolocation', function () {
-                var geolocation = new AMap.Geolocation({
+        template: '<div id="amap-container" style="height: 500px;"></div>',
+        replace: true,
+        link: function () {
+            amap = new AMap.Map('amap-container');
+            amap.plugin('AMap.Geolocation', function () {
+                geolocation = new AMap.Geolocation({
                     enableHighAccuracy: true,//是否使用高精度定位，默认:true
                     timeout: 10000,          //超过10秒后停止定位，默认：无穷大
                     maximumAge: 0,           //定位结果缓存0毫秒，默认：0
@@ -26,21 +30,34 @@ export default function AmapDirective() {
                     showMarker: true,        //定位成功后在定位到的位置显示点标记，默认：true
                     showCircle: true,        //定位成功后用圆圈表示定位精度范围，默认：true
                     panToLocation: true,     //定位成功后将定位到的位置作为地图中心点，默认：true
-                    zoomToAccuracy:true      //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+                    zoomToAccuracy: true      //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
                 });
-                map.addControl(geolocation);
+                amap.addControl(geolocation);
                 geolocation.getCurrentPosition();
-                AMap.event.addListener(geolocation, 'complete', onComplete);//返回定位信息
-                AMap.event.addListener(geolocation, 'error', onError);      //返回定位出错信息
+                geolocation.on('complete', onComplete);//返回定位信息
+                geolocation.on('error', onError);      //返回定位出错信息
             });
-        }
+        },
+        controller: ['$scope', function ($scope) {
+            $scope.$on("$destroy", function () {
+                if (amap) {
+                    amap.destroy();
+                    amap = null;
+                }
+                if (geolocation) {
+                    geolocation.off('complete', onComplete);
+                    geolocation.off('error', onError);
+                    geolocation = null;
+                }
+            });
+        }]
     };
 
     function onComplete() {
 
     }
 
-    function onError() {
-
+    function onError(err) {
+        $log.log(err)
     }
 }
