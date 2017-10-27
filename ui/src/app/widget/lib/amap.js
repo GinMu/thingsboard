d3900d42cad17cfa3aadef8126f548586020361b
GinMu <gin.musheng@gmail.com>
2017-10-27 11:08:21 +0800

    Fixed amap bug

--- ui/src/app/widget/lib/amap.js
+++ ui/src/app/widget/lib/amap.js
@@ -7,7 +7,7 @@
 import './amap.scss';
 import AMap from 'AMap';
 
-AMap.Bounds.prototype.tbExtend = function (obj) {
+AMap.Bounds.prototype.extendLatLng = function (obj) {
     var ne = this.eb,
         sw = this.mb;
     var ne2,
@@ -26,8 +26,8 @@ AMap.Bounds.prototype.tbExtend = function (obj) {
     }
 
     if (!ne || !sw) {
-        this.eb = ne2;
-        this.mb = sw2;
+        this.eb = angular.copy(ne2);
+        this.mb = angular.copy(sw2);
         this.southwest = this.mb;
         this.northeast = this.eb;
     } else {
@@ -52,41 +52,10 @@ export default class TbAMap {
         this.dontFitMapBounds = dontFitMapBounds;
         this.minZoomLevel = minZoomLevel;
         this.tooltips = [];
-        this.geolocation = null;
 
         if (initCallback) {
             setTimeout(initCallback, 0);
         }
-    }
-
-    init() {
-        this.map.plugin('AMap.Geolocation', () => {
-            this.geolocation = new AMap.Geolocation({
-                enableHighAccuracy: true,//是否使用高精度定位，默认:true
-                timeout: 10000,          //超过10秒后停止定位，默认：无穷大
-                maximumAge: 0,           //定位结果缓存0毫秒，默认：0
-                convert: true,           //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
-                showButton: true,        //显示定位按钮，默认：true
-                buttonPosition: 'LB',    //定位按钮停靠位置，默认：'LB'，左下角
-                buttonOffset: new AMap.Pixel(10, 20),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
-                showMarker: true,        //定位成功后在定位到的位置显示点标记，默认：true
-                showCircle: true,        //定位成功后用圆圈表示定位精度范围，默认：true
-                panToLocation: true,     //定位成功后将定位到的位置作为地图中心点，默认：true
-                zoomToAccuracy: true      //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
-            });
-            this.map.addControl(this.geolocation);
-            this.geolocation.getCurrentPosition();
-            this.geolocation.on('complete', this.onComplete.bind(this));//返回定位信息
-            this.geolocation.on('error', this.onError.bind(this));//定位出错
-        });
-    }
-
-    onComplete(res) {
-        console.log(res) //eslint-disable-line
-    }
-
-    onError(err) {
-        console.log(err) //eslint-disable-line
     }
 
     destroy() {
@@ -94,11 +63,6 @@ export default class TbAMap {
         if (this.map) {
             this.map.destroy();
             this.map = null;
-        }
-        if (this.geolocation) {
-            this.geolocation.off('complete', this.onComplete);
-            this.geolocation.off('error', this.onError);
-            this.geolocation = null;
         }
     }
 
@@ -281,7 +245,7 @@ export default class TbAMap {
 
     extendBoundsWithMarker(bounds, marker) {
         console.log('extendBoundsWithMarker:', bounds, marker.getPosition());
-        bounds.tbExtend(marker.getPosition());
+        bounds.extendLatLng(marker.getPosition());
     }
 
     getMarkerPosition(marker) {
@@ -311,7 +275,7 @@ export default class TbAMap {
     extendBounds(bounds, polyline) {
         console.log('扩展bounds:', bounds, polyline.getBounds());
         if (polyline && polyline.getPath()) {
-            bounds.tbExtend(polyline.getBounds());
+            bounds.extendLatLng(polyline.getBounds());
         }
     }
 
